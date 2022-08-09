06163f2e7bcb99cb5da77268cdd84e67c77cc75b
Andrew5525 <109211005+Andrew5525@users.noreply.github.com>
2022-08-09 18:33:21 -0500

    pre

---
+++ js/lightbox.js
@@ -0,0 +1,20 @@
+const imagenes = document.querySelector('.Image-Tablas')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.image-light')
+console.log(imagenes)
+console.log(imagenesLight)
+
+imagenesLight.addEventListener('click',()=>{
+    imagenes.classList.toggle("spread")
+ })
+
+ window.addEventListener('click', e=>{
+    if(imagenes.classList.contains('spread')
+    && e.target != imagenes && e.target != imagenesLight){
+        imagenes.classList.toggle('spread')
+    }
+})
+
+
+
+     
